d4b496ba82d3da3daf8a1acbaa36404c7a2ee596
Sitthisak <sitthisak.srs@spumail.net>
2025-05-30 15:26:00 +0700

    modified:   assets/css/styles.css 	modified:   assets/js/script.js 	modified:   index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
-  const images = document.querySelectorAll('.image-fade');
+  const images = [...document.querySelectorAll('.image-fade')];
+  const baseDelay = 500;
 
-  const delays = [0, 500, 1000, 1500]; // เวลาแสดงแต่ละรูปเป็นมิลลิวินาที (ms)
-
-  images.forEach((img, index) => {
-    setTimeout(() => {
-      img.classList.add('opacity-100', 'translate-y-0');
-    }, delays[index] || 0);
-  });
+  // เรียงลำดับตาม data-sequence
+  images
+    .sort((a, b) => a.dataset.sequence - b.dataset.sequence)
+    .forEach((img, index) => {
+      setTimeout(() => {
+        img.classList.add('opacity-100', 'translate-y-0');
+      }, index * baseDelay);
+    });
 });
